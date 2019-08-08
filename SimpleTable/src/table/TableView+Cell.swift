03f1c150e031f7ad0bc390c8e15b3abc70c93cc4
andre.jorgensen <andre.jorgensen@tidal.com>
2019-08-08 09:48:50 +0200

    Add Cell types

--- SimpleTable/src/table/TableView+Cell.swift
+++ SimpleTable/src/table/TableView+Cell.swift
@@ -51,9 +51,12 @@ extension TableView {
    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
       let id: String = rowData[indexPath.row]
       _ = id
+      Swift.print("tapped")
+      self.contentOffset.y += 32
 //      let vc = TertiaryDetailVC.init(id: id)
 //      self.navigationController()?.pushViewController(vc, animated: true) // present(navController, animated: true, completion: {Swift.print("completed the transition")})
    }
+//   tableheader
 }
 /**
  * Cell

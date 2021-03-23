ec346c6fa931ca90e07714033d1ceb08c8254bb1
eonist <30n1st@gmail.com>
2021-03-23 14:42:10 +0100

    Clean up

--- SimpleTable/src/table/TableView+Config.swift
+++ SimpleTable/src/table/TableView+Config.swift
@@ -9,7 +9,7 @@ extension TableView {
    func registerCells() {
       self.register(CustomCell.self)
       self.register(AnotherCustomCell.self)
-      self.register(types: [CustomCell.self, AnotherCustomCell.self])
+      self.register([CustomCell.self, AnotherCustomCell.self])
    }
    /**
     * Config table
@@ -22,10 +22,5 @@ extension TableView {
       // UI
       self.contentInset = .zero
       self.scrollIndicatorInsets = self.contentInset
-   }
-}
-extension UITableView {
-   public func register<T: UITableViewCell>(types: [T.Type]) where T: ReusableCellKind {
-      types.forEach { register($0.self, forCellReuseIdentifier: $0.defaultReuseIdentifier) }
    }
 }
